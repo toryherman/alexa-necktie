48aed386a517b98310deb2f282f7616b6857001d
Tory Herman <tory.herman12@gmail.com>
2017-04-08 23:17:40 -0500

    add NextStepIntent

--- src/recipes.js
+++ src/recipes.js
@@ -1,17 +1,18 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
 module.exports = {
-    "RECIPE_EN_US" : {
-        "snow golem": "A snow golem can be created by placing a pumpkin on top of  two snow blocks on the ground.",
-        "pillar quartz block": "A pillar of quartz can be obtained by placing a block of quartz on top of a block of quartz in mine craft.",
-        "firework rocket": "A firework rocket can be crafted by placing a firework star in the left middle square, a piece of paper in the center square, and gunpowder in the right middle square in a crafting table. Similar to a firework star, a firework rocket can have more gunpowder added in the bottom row to increase the duration of a rocket.",
-        "rabbit stew": "Rabbit stew can be crafted by placing cooked rabbit in the top middle square, a carrot in the middle left square, a baked potato in the center square, any type of mushroom in the middle right square, and a bowl in the bottom middle square.",
-        "cauldron": "A cauldron can be created by placing iron ingots in all squares but the top middle and very center squares in a crafting table.",
-        "stone shovel": "All types of shovels can be crafted by placing the desired material in the top middle square, and then sticks in the two squares directly beneath that in a crafting table.",
-        "red carpet": "Any type of carpet can be crafted by placing two wool, of the same color, next to each other in a crafting window.",
-        "book and quill": "A book and quill can be crafted by placing a book in the middle left square, an ink sac in the very center square, and a feather in the bottom middle square in a crafting table.",
-        "item frame": "An item frame can be crafted by placing leather in the very center square, and eight sticks surrounding it.",
-        "map": "A map can be crafted by placing a compass in the middle square and eight pieces of paper surrounding it.",
-        "sticky piston": "A sticky piston can be crafted by placing a slime ball on top of a piston in a crafting window",
-        "bread": "Bread can be crafted by placing three wheat across a row in a crafting table.",
-        "wooden pick ax": "All types of pick axs can be crafted by placing three of the desired material across the top row, and then placing sticks in the center and bottom middle squares in a crafting table."
-    }
+  RECIPE_EN_US: {
+    'windsor knot':
+      [
+        'Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
+        'Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
+        'Cross the wide end over the narrow end to the left.',
+        'Bring the wide end back behind the narrow end and up between the cross and your collar.',
+        'Bring the wide end down and cross underneath the narrow end from left to right. At this point the back of the wide end should be facing forward.',
+        'Pass the wide end once again between the knot and your neck.',
+        'Cross the wide end over the narrow end from right to left, then up behind the knot.',
+        'Finally, pass the wide end down through the loop created in the front of the knot.',
+        'Tighten as necessary by pulling down the wide end.'
+      ]
+  }
 };
