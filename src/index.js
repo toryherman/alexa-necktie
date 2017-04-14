9edb2e05720868e65fd594fbf2d4c6014c2c50bd
Tory Herman <tory.herman12@gmail.com>
2017-04-14 14:46:41 -0500

    add states

--- src/index.js
+++ src/index.js
@@ -17,101 +17,116 @@ const APP_ID = "amzn1.ask.skill.d5a45154-b2f3-4dd5-bfd5-f916840e2b1f";
 let recipe;
 let itemName;
 
-const handlers = {
-    'NewSession': function () {
-        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
-        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
-    },
-    'RecipeIntent': function () {
-        this.attributes.i = 0;
-        let itemSlot = this.event.request.intent.slots.Item;
-
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        if (itemName == 'windsor' | itemName == 'full windsor' | itemName == 'full windsor knot' | itemName == 'double windsor' | itemName == 'double windsor knot') {
-            itemName = 'windsor knot';
-        } else if (itemName == 'half windsor' | itemName == 'single windsor' | itemName == 'single windsor knot') {
-            itemName = 'half windsor knot';
-        } else if (itemName == 'pratt' | itemName == 'shelby' | itemName == 'shelby knot' | itemName == 'pratt shelby' | itemName == 'pratt shelby knot') {
-            itemName = 'pratt knot';
-        } else if (itemName == '4 in hand' | itemName == '4 in hand knot' | itemName == 'four in hand' | itemName == 'simple' | itemName == 'simple knot' | itemName == 'schoolboy' | itemName == 'schoolboy knot') {
-            itemName = 'four in hand knot';
-        }
-
-        const myRecipes = this.t('RECIPES');
-        recipe = myRecipes[itemName];
-
-        this.emit('GetRecipe');
-    },
-    'GetRecipe': function () {
-        if (recipe) {
-            let i = this.attributes.i;
-            if (recipe[i]) {
-                this.attributes.speechOutput = recipe[i];
-                this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
-                this.emit(':ask', recipe[i], this.attributes.repromptSpeech);
-            } else if (i == recipe.length){
-                const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
-                this.attributes.i = 0;
-                this.emit(':tell', this.t('RECIPE_COMPLETE') + itemName); // cardTitle, cardContent, imageObj
-            } else {
-                this.emit(':ask', this.t('NO_STEP') + ' ' + this.t('HELP_MESSAGE'), this.t('HELP_REPROMPT'));
-                // this.emit('NewSession');
-            }
-        } else {
-            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
-            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
-            if (itemName) {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
-            } else {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes.speechOutput = speechOutput;
-            this.attributes.repromptSpeech = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
-    'NextStepIntent': function () {
-        this.attributes.i++;
-        this.emit('GetRecipe');
-    },
-    'StartOverIntent': function () {
-        this.attributes.i = 0;
-        this.emit('GetRecipe');
-    },
-    'MainMenuIntent': function () {
-        this.emit('NewSession');
-    },
-    'GoToIntent': function () {
-        this.attributes.i = this.event.request.intent.slots.Number.value - 1;
-        this.emit('GetRecipe');
-    },
-    'AMAZON.HelpIntent': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
-        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
-    },
-    'AMAZON.RepeatIntent': function () {
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit('SessionEndedRequest');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit('SessionEndedRequest');
-    },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
+const states = {
+    STARTMODE: '_STARTMODE',
+    STEPMODE: '_STEPMODE' // while going through instructions
 };
+
+const newSessionHandlers = {
+  'NewSession': function () {
+      this.handler.state = states.STARTMODE;
+      this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+      // If the user either does not reply to the welcome message or says something that is not
+      // understood, they will be prompted again with this text.
+      this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
+      this.emitWithState(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+  },
+  'AMAZON.HelpIntent': function () {
+      this.attributes.speechOutput = this.t('HELP_MESSAGE');
+      this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+      this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+  },
+  'AMAZON.StopIntent': function () {
+      this.emit('SessionEndedRequest');
+  },
+  'AMAZON.CancelIntent': function () {
+      this.emit('SessionEndedRequest');
+  },
+  'SessionEndedRequest': function () {
+      this.emit(':tell', this.t('STOP_MESSAGE'));
+  }
+};
+
+const startModeHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+  'RecipeIntent': function () {
+      this.handler.state = states.STEPMODE;
+      this.attributes.i = 0;
+      let itemSlot = this.event.request.intent.slots.Item;
+
+      if (itemSlot && itemSlot.value) {
+          itemName = itemSlot.value.toLowerCase();
+      }
+
+      if (itemName == 'windsor' | itemName == 'full windsor' | itemName == 'full windsor knot' | itemName == 'double windsor' | itemName == 'double windsor knot') {
+          itemName = 'windsor knot';
+      } else if (itemName == 'half windsor' | itemName == 'single windsor' | itemName == 'single windsor knot') {
+          itemName = 'half windsor knot';
+      } else if (itemName == 'pratt' | itemName == 'shelby' | itemName == 'shelby knot' | itemName == 'pratt shelby' | itemName == 'pratt shelby knot') {
+          itemName = 'pratt knot';
+      } else if (itemName == '4 in hand' | itemName == '4 in hand knot' | itemName == 'four in hand' | itemName == 'simple' | itemName == 'simple knot' | itemName == 'schoolboy' | itemName == 'schoolboy knot') {
+          itemName = 'four in hand knot';
+      }
+
+      const myRecipes = this.t('RECIPES');
+      recipe = myRecipes[itemName];
+
+      this.emitWithState('GetRecipe');
+  }
+});
+
+const stepModeHandlers = Alexa.CreateStateHandler(states.STEPMODE, {
+  'GetRecipe': function () {
+      if (recipe) {
+          let i = this.attributes.i;
+          if (recipe[i]) {
+              this.attributes.speechOutput = recipe[i];
+              this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
+              this.emit(':ask', recipe[i], this.attributes.repromptSpeech);
+          } else if (i == recipe.length){
+              const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+              this.attributes.i = 0;
+              this.emit(':tell', this.t('RECIPE_COMPLETE') + itemName); // cardTitle, cardContent, imageObj
+          } else {
+            this.attributes.speechOutput = this.t('NO_STEP') + ' ' + this.t('HELP_MESSAGE');
+            this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+              this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+          }
+      } else {
+          let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
+          const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
+          if (itemName) {
+              speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+          } else {
+              speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+          }
+          speechOutput += repromptSpeech;
+
+          this.attributes.speechOutput = speechOutput;
+          this.attributes.repromptSpeech = repromptSpeech;
+
+          this.emit(':ask', speechOutput, repromptSpeech);
+      }
+  },
+  'NextStepIntent': function () {
+      this.attributes.i++;
+      this.emit('GetRecipe');
+  },
+  'StartOverIntent': function () {
+      this.attributes.i = 0;
+      this.emit('GetRecipe');
+  },
+  'MainMenuIntent': function () {
+      this.handler.state = '';
+      this.emitWithState('NewSession');
+  },
+  'GoToIntent': function () {
+      this.attributes.i = this.event.request.intent.slots.Number.value;
+      this.emit('GetRecipe');
+  },
+  'AMAZON.RepeatIntent': function () {
+      this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+  }
+});
 
 const languageStrings = {
     'en-US': {
@@ -125,7 +140,7 @@ const languageStrings = {
             HELP_MESSAGE: "You can ask questions such as, how do I tie a windsor knot, or, you can say exit...Now, what can I help you with?",
             HELP_REPROMPT: "You can say things like, how do I tie a windsor knot, or you can say exit...Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!',
-            RECIPE_REPEAT_MESSAGE: 'You can say repeat to hear the step again, or next to move on.',
+            RECIPE_REPEAT_MESSAGE: 'You can say repeat to hear the step again, or okay to move on.',
             RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
             RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
             RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
@@ -141,6 +156,6 @@ exports.handler = (event, context) => {
     alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(newSessionHandlers, startModeHandlers, stepModeHandlers);
     alexa.execute();
 };
--- src/recipes.js
+++ src/recipes.js
@@ -4,56 +4,58 @@ module.exports = {
   RECIPE_EN_US: {
     'windsor knot':
       [
-        'A windsor knot, also known as a full or double windsor, is a wide, symmetrical knot. When you are ready to begin, you can say okay or next.',
-        'Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
-        'Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
-        'Cross the wide end over the narrow end to the left.',
-        'Bring the wide end back behind the narrow end and up between the cross and your collar.',
-        'Bring the wide end down and cross underneath the narrow end from left to right. At this point the back of the wide end should be facing forward.',
-        'Pass the wide end once again between the knot and your neck.',
-        'Cross the wide end over the narrow end from right to left, then up behind the knot.',
-        'Finally, pass the wide end down through the loop created in the front of the knot.',
-        'Tighten as necessary by pulling down the wide end.'
+        'A windsor knot, also known as a full or double windsor, is a wide, symmetrical knot. When you are ready to move ahead, say okay.',
+        'Step 1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
+        'Step 2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
+        'Step 3. Cross the wide end over the narrow end to the left.',
+        'Step 4. Bring the wide end back behind the narrow end and up between the cross and your collar.',
+        'Step 5. Bring the wide end down and cross underneath the narrow end from left to right. At this point the back of the wide end should be facing forward.',
+        'Step 6. Pass the wide end once again between the knot and your neck.',
+        'Step 7. Cross the wide end over the narrow end from right to left, then up behind the knot.',
+        'Step 8. Finally, pass the wide end down through the loop created in the front of the knot.',
+        'Step 9. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.'
       ],
     'four in hand knot':
       [
-        'A four in hand knot, also known as a simple or schoolboy knot, is a narrow, asymmetrical knot. When you are ready to begin, you can say okay or next.',
-        'Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
-        'Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
-        'Cross the wide end over the narrow end to the left.',
-        'Bring the wide end back behind the narrow end to the right.',
-        'Once more, cross the wide end over the narrow end to the left, then up between the knot and your collar.',
-        'Pass the wide end down through the loop created in the front of the knot.',
-        'Tighten as necessary by pulling down the wide end.'
+        'A four in hand knot, also known as a simple or schoolboy knot, is a narrow, asymmetrical knot. When you are ready to move ahead, say okay.',
+        'Step 1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
+        'Step 2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
+        'Step 3. Cross the wide end over the narrow end to the left.',
+        'Step 4. Bring the wide end back behind the narrow end to the right.',
+        'Step 5. Once more, cross the wide end over the narrow end to the left, then up between the knot and your collar.',
+        'Step 6. Pass the wide end down through the loop created in the front of the knot.',
+        'Step 7. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.'
       ],
     'half windsor knot':
       [
-        'A half windsor knot, also known as a single windsor, is a medium width, symmetrical knot. When you are ready to begin, you can say okay or next.',
-        'Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
-        'Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
-        'Cross the wide end over the narrow end to the left.',
-        'Bring the wide end back behind the narrow end to the right.',
-        'Bring the wide end over and down through the space between your knot and collar. The back of the wide end should be facing forward.',
-        'Wrap the wide end back over the narrow end from left to right.',
-        'Pass the wide end under and up through the space between your knot and collar.',
-        'Finally, pass the wide end down through the loop created in the front of the knot.'
+        'A half windsor knot, also known as a single windsor, is a medium width, symmetrical knot. When you are ready to move ahead, say okay.',
+        'Step 1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.',
+        'Step 2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
+        'Step 3. Cross the wide end over the narrow end to the left.',
+        'Step 4. Bring the wide end back behind the narrow end to the right.',
+        'Step 5. Bring the wide end over and down through the space between your knot and collar. The back of the wide end should be facing forward.',
+        'Step 6. Wrap the wide end back over the narrow end from left to right.',
+        'Step 7. Pass the wide end under and up through the space between your knot and collar.',
+        'Step 8. Finally, pass the wide end down through the loop created in the front of the knot.',
+        'Step 9. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.'
       ],
     'pratt knot':
       [
-        'A pratt knot, also known as a shelby, is a medium width, symmetrical knot. When you are ready to begin, you can say okay or next.',
-        'Begin with the backside of your tie facing forward, the wide end of the tie on your right hand side, and the narrow end on the left.',
-        'Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
-        'Cross the wide end under the narrow end to the left.',
-        'Bring the wide end over and down through the space between your knot and collar.',
-        'Cross the wide end over the narrow end to the right.',
-        'Pass the wide end under and up through the space between your knot and collar.',
-        'Finally, pass the wide end down through the loop created in the front of the knot.'
+        'A pratt knot, also known as a shelby, is a medium width, symmetrical knot. When you are ready to move ahead, say okay.',
+        'Step 1. Begin with the back side of your tie facing forward, the wide end of the tie on your right hand side, and the narrow end on the left.',
+        'Step 2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.',
+        'Step 3. Cross the wide end under the narrow end to the left.',
+        'Step 4. Bring the wide end over and down through the space between your knot and collar.',
+        'Step 5. Cross the wide end over the narrow end to the right.',
+        'Step 6. Pass the wide end under and up through the space between your knot and collar.',
+        'Step 7. Finally, pass the wide end down through the loop created in the front of the knot.'
+        'Step 8. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.'
       ]
   },
   CARDS: {
-    'windsor knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end and up between the cross and your collar.\n5. Bring the wide end down and cross underneath the narrow end from left to right. At this point the back of the wide end should be facing forward.\n6. Pass the wide end once again between the knot and your neck.\n7. Cross the wide end over the narrow end from right to left, then up behind the knot.\n8. Finally, pass the wide end down through the loop created in the front of the knot.\n9. Tighten as necessary by pulling down the wide end.',
-    'four in hand knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end to the right.\n5. Once more, cross the wide end over the narrow end to the left, then up between the knot and your collar.\n6. Pass the wide end down through the loop created in the front of the knot.\n7. Tighten as necessary by pulling down the wide end.',
-    'half windsor knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end to the right.\n5. Bring the wide end over and down through the space between your knot and collar. The back of the wide end should be facing forward.\n6. Wrap the wide end back over the narrow end from left to right.\n7. Pass the wide end under and up through the space between your knot and collar.\n8. Finally, pass the wide end down through the loop created in the front of the knot.',
-    'pratt knot': '1.	Begin with the backside of your tie facing forward, the wide end of the tie on your right hand side, and the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end under the narrow end to the left.\n4. Bring the wide end over and down through the space between your knot and collar.\n5. Cross the wide end over the narrow end to the right.\n6. Pass the wide end under and up through the space between your knot and collar.\n7. Finally, pass the wide end down through the loop created in the front of the knot.'
+    'windsor knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end and up between the cross and your collar.\n5. Bring the wide end down and cross underneath the narrow end from left to right. At this point the back of the wide end should be facing forward.\n6. Pass the wide end once again between the knot and your neck.\n7. Cross the wide end over the narrow end from right to left, then up behind the knot.\n8. Finally, pass the wide end down through the loop created in the front of the knot.\n9. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.',
+    'four in hand knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end to the right.\n5. Once more, cross the wide end over the narrow end to the left, then up between the knot and your collar.\n6. Pass the wide end down through the loop created in the front of the knot.\n7. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.',
+    'half windsor knot': '1. Begin with the wide end of the tie on your right hand side, the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end over the narrow end to the left.\n4. Bring the wide end back behind the narrow end to the right.\n5. Bring the wide end over and down through the space between your knot and collar. The back of the wide end should be facing forward.\n6. Wrap the wide end back over the narrow end from left to right.\n7. Pass the wide end under and up through the space between your knot and collar.\n8. Finally, pass the wide end down through the loop created in the front of the knot.\n9.Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.',
+    'pratt knot': '1.	Begin with the backside of your tie facing forward, the wide end of the tie on your right hand side, and the narrow end on the left.\n2. Align the tip of the narrow end slightly above your belly button. You may need to adjust this alignment based on both your size and the length of the tie.\n3. Cross the wide end under the narrow end to the left.\n4. Bring the wide end over and down through the space between your knot and collar.\n5. Cross the wide end over the narrow end to the right.\n6. Pass the wide end under and up through the space between your knot and collar.\n7. Finally, pass the wide end down through the loop created in the front of the knot.\n8. Tighten the knot by pulling down the wide end. Adjust by holding the narrow end and sliding the knot upward.'
   }
 };
